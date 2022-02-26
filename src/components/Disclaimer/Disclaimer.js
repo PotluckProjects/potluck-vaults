91be0ac11c94577b7a38c1037ece46d6fd502bea
root <cyberarmenia@gmail.com>
2022-02-26 21:34:19 +0100

    first commit

---
+++ src/components/Disclaimer/Disclaimer.js
@@ -0,0 +1,26 @@
+import React, { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import styles from './styles';
+
+const useStyles = makeStyles(styles);
+
+const Disclaimer = () => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  return (
+    <Grid container item className={classes.root} justifyContent="center">
+      <Typography className={classes.disclaimer}>
+        {t('Disclaimer')}
+        <br />
+        <br />
+      </Typography>
+    </Grid>
+  );
+};
+
+export default memo(Disclaimer);
---
+++ src/features/configure/vault/fantom_pools.js
@@ -0,0 +1,37 @@
+export const fantomPools = [
+    {
+      id: 'fantom-potluck-fang',
+      logo: 'single-assets/BIFI.png',
+      name: 'FANG',
+      token: 'FANG',
+      tokenDescription: 'Beefy.Finance',
+      tokenAddress: '0x49894fCC07233957c35462cfC3418Ef0CC26129f',
+      tokenDecimals: 18,
+      tokenDescriptionUrl: '#',
+      earnedToken: 'mooFANG',
+      earnedTokenAddress: '0xf7E546e3F11B16cCe33E327FCb9B3c4097d87aaE',
+      earnContractAddress: '0xf7E546e3F11B16cCe33E327FCb9B3c4097d87aaE',
+      pricePerFullShare: 1,
+      tvl: 0,
+      oracle: 'tokens',
+      oracleId: 'FANG',
+      oraclePrice: 0,
+      depositsPaused: false,
+      status: 'active',
+      platform: 'Beefy.Finance',
+      assets: ['FANG'],
+      risks: [
+        'COMPLEXITY_LOW',
+        'BATTLE_TESTED',
+        'IL_NONE',
+        'MCAP_MEDIUM',
+        'AUDIT',
+        'CONTRACTS_VERIFIED',
+      ],
+      stratType: 'SingleStake',
+      withdrawalFee: '0',
+      buyTokenUrl:
+        'https://spiritswap.finance/swap?outputCurrency=0x49894fCC07233957c35462cfC3418Ef0CC26129f',
+      createdAt: 1622574935,
+    }
+  ];
