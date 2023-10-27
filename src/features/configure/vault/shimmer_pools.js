b5d1bc1a2e8744a92a47a0ecc7ebf24fcbf7678b
m4r0z <99085398+m4r0z@users.noreply.github.com>
2023-10-28 00:42:47 +0400

    Update shimmer_pools.js

--- src/features/configure/vault/shimmer_pools.js
+++ src/features/configure/vault/shimmer_pools.js
@@ -1,1 +1,36 @@
-export const shimmerPools = [];
+export const shimmerPools = [
+  {
+    id: "shimmer-shimmer-lum",
+    name: "SMR-LUM LP",
+    token: "SMR-LUM LP",
+    tokenDescription: "SMR LUM",
+    tokenAddress: "0x95f00a7125EC3D78d6B2FCD6FFd9989941eF25fC",
+    tokenDecimals: 18,
+    tokenDescriptionUrl: "#",
+    earnedToken: "ACR-SMR-LUM",
+    earnedTokenAddress: "0x11C0A85EAa5b8a703267231CA5C0e32038C0ffcf",
+    earnContractAddress: "0x11C0A85EAa5b8a703267231CA5C0e32038C0ffcf",
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: "lps",
+    oracleId: "shimmer-shimmer-lum",
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: "active",
+    platform: "shimmersea",
+    withdrawalFee: "0%",
+    assets: ["shimmer", "LUM"],
+    risks: [
+      "COMPLEXITY_LOW",
+      "BATTLE_TESTED",
+      "IL_LOW",
+      "AUDIT",
+      "CONTRACTS_VERIFIED",
+    ],
+    stratType: "StratLP",
+    addLiquidityUrl:
+      "https://shimmersea.finance/add/0x1074010000000000000000000000000000000000/0x4794Aeafa5Efe2fC1F6f5eb745798aaF39A81D3e",
+    buyTokenUrl:
+      "https://shimmersea.finance/swap?outputCurrency=0x4794Aeafa5Efe2fC1F6f5eb745798aaF39A81D3e",
+  },
+];
